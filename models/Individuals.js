6b0b2446f9c9d3af3d772f9fbbbd6bbca283b353
dcartist <pbannerman13@gmail.com>
2022-04-09 20:55:04 -0400

    adding timestamp

--- models/Individuals.js
+++ models/Individuals.js
@@ -1,6 +1,11 @@
 const mongoose = require('../db/connection')
 
 var Schema = mongoose.Schema;
+const opts = {
+    // Make Mongoose use Unix time (seconds since Jan 1, 1970)
+    timestamps: { currentTime: () => Math.floor(Date.now() / 1000) },
+  };
+  
 var Individual = new Schema({
   name: { type: String },
   email: { type: String },
@@ -11,7 +16,8 @@ var Individual = new Schema({
   symptoms: { type: String },
   covid: { type: String },
   
-});
+} ,
+opts);
 
 
 let individual = mongoose.model('Individual', Individual)
